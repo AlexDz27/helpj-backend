018eb615b0c10d5eb6660efc24152383718324e2
Alex Dzyuba <alexeydzyuba27@gmail.com>
2023-02-07 11:06:00 +0100

    csr update

--- server.js
+++ server.js
@@ -7,25 +7,30 @@ const database = JSON.parse(databaseRaw)
 
 const server = http.createServer((request, response) => {
   response.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
-  response.setHeader('Content-Type', 'text/plain');
 
-  if (request.method === 'POST') {
-    let likesCountUpdated
-    const body = []
-    request.on('data', (chunk) => {
-      body.push(chunk)
-    }).on('end', () => {
-      likesCountUpdated = Buffer.concat(body).toString()
-
-      // Write to database
-      database.likesCount = likesCountUpdated
-      const databaseRawToWrite = JSON.stringify(database)
-      fs.writeFileSync('database.json', databaseRawToWrite)
-
-      response.end('Updated likes count: ' + database.likesCount);
-    })
-  } else {
+  if (request.url === '/') {
+    response.end('Hi from server!');
+  } else if (request.url === '/likes') {
     response.end(database.likesCount);
+  } else if (request.url === '/update-likes') {
+    if (request.method === 'POST') {
+      let likesCountUpdated
+      const body = []
+      request.on('data', (chunk) => {
+        body.push(chunk)
+      }).on('end', () => {
+        likesCountUpdated = Buffer.concat(body).toString()
+  
+        // Write to database
+        database.likesCount = likesCountUpdated
+        const databaseRawToWrite = JSON.stringify(database)
+        fs.writeFileSync('database.json', databaseRawToWrite)
+  
+        response.end('Updated likes count: ' + database.likesCount);
+      })
+    } else {
+      response.end('You should make this request with POST instead of GET');
+    }
   }
 });
 const port = 8000;
